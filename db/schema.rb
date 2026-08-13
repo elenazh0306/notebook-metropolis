d1e1ed0e46cb41cbb20cc1e646d71208f20a081e
Elena <ellena.zhukof@gmail.com>
2026-08-13 14:34:45 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_060514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "bookmarks", force: :cascade do |t|
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.bigint "list_id", null: false
-    t.bigint "movie_id", null: false
-    t.datetime "updated_at", null: false
-    t.index ["list_id"], name: "index_bookmarks_on_list_id"
-    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
-  end
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -45,12 +36,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_060514) do
     t.index ["category_id"], name: "index_citizens_on_category_id"
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "name"
-    t.datetime "updated_at", null: false
-  end
-
   create_table "messages", force: :cascade do |t|
     t.bigint "citizen_id", null: false
     t.text "content"
@@ -58,15 +43,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_060514) do
     t.string "role"
     t.datetime "updated_at", null: false
     t.index ["citizen_id"], name: "index_messages_on_citizen_id"
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "overview"
-    t.string "poster_url"
-    t.integer "rating"
-    t.string "title"
-    t.datetime "updated_at", null: false
   end
 
   create_table "notes", force: :cascade do |t|
@@ -92,10 +68,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_060514) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookmarks", "lists"
-  add_foreign_key "bookmarks", "movies"
+ 
   add_foreign_key "categories", "users"
   add_foreign_key "citizens", "categories"
   add_foreign_key "messages", "citizens"
