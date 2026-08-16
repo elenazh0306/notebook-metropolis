3c03dadd7cb1a0e38738c01588c5024096e2f603
Thomas Davidsen <260642635+Thomas-H-D@users.noreply.github.com>
2026-08-16 22:16:19 +0900

    forked the turbo stream in our room and added subfolders to notes model, all to create a seperate area to the room where you can store notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,12 @@
 class Note < ApplicationRecord
   belongs_to :category
+
+  # We ensures default subfolder is "notice_board"
+  after_initialize :set_default_subfolder, if: :new_record?
+
+  private
+
+  def set_default_subfolder
+    self.subfolder ||= "notice_board" # Conditional Assignment - to assign to "notice_board" if nil
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_12_060514) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_15_053934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -101,6 +101,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_060514) do
     t.bigint "category_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
+    t.string "subfolder"
     t.string "title"
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_notes_on_category_id"
