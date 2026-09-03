d0d116eb05635af77740dbe3508b1bddd09c14e3
Thomas Davidsen <260642635+Thomas-H-D@users.noreply.github.com>
2026-09-03 21:47:35 +0900

    moved hotspot creation to category model to create them during database seeding

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,6 +6,8 @@ class Category < ApplicationRecord
   validates :name, presence: { message: "You forgot the name!"}
   validates :sprite_image, presence: { message: "Pick a building!"}
   accepts_nested_attributes_for :notes
+  # trigger hotspot method after creating category
+  after_create_commit :hotspot
 
   # sets the building sprite on the map grid
   def building_sprite
@@ -31,5 +33,35 @@ class Category < ApplicationRecord
 
   def system_category?
     name.downcase == "trash" # Skips uniqueness validation if the name is "trash"
+  end
+
+  def hotspot
+    # Corkboard Hotspot
+    self.notes.create!(
+      title: "Corkboard introduction",
+      content: "📌 This is your corkboard! Start with your first post-it note right now!",
+      hotspot_type: "notice_board"
+    )
+
+    # Bookcase Hotspot
+    self.notes.create!(
+      title: "Library introduction",
+      content: "📚 This is your bookshelf! Do you know the best part? You are the author of all of them!",
+      hotspot_type: "bookcase"
+    )
+
+    # Laptop Hotspot
+    self.notes.create!(
+      title: "Terminal Workspace Logs",
+      content: "💾 Time to enter the matrix! Start your journey right now!",
+      hotspot_type: "laptop"
+    )
+
+    # Posters Hotspot
+    self.notes.create!(
+      title: "Inspirational Quote",
+      content: "🌠 Turn your pictures into motivational posts! Just! Do! It!",
+      hotspot_type: "posters"
+    )
   end
 end
